1608a7fb4be36d5ee446d92a53a3822e474fb440
Eshwar Muneshar <Emuneshar@gmail.com>
2023-04-28 17:15:30 -0400

    Added input for the hint

---
+++ Hangman w M & H.py
@@ -0,0 +1,13 @@
+import os
+
+rightLetters = []
+wrongLetters = []
+
+print("Welcome to Hangman\n")
+print("One player will enter a secret word\n")
+print("The second player will have to guess the secret word\n")
+
+secretWord = input("What is the secret word?\n")
+
+hint = input("Please enter a hint\n")
+
